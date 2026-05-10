31aacbed0566862c0745660b747629af35fe6d1d
infoagrotechinsights-cmd <infoagrotechinsights@gmail.com>
2026-05-11 00:31:22 +0300

    Update rag_engine.py

--- core/rag_engine.py
+++ core/rag_engine.py
@@ -1,26 +1,43 @@
 from core.vector_db import search
 from core.llm_engine import ask_gpt
 
+
 def rag_answer(query):
 
     results = search(query)
 
-    context = "\n".join(results["documents"][0])
+    documents = results.get("documents", [[]])
+
+    retrieved_docs = documents[0] if documents else []
+
+    context = "\n\n".join(retrieved_docs)
 
     prompt = f"""
-You are an academic literary researcher.
+You are an expert academic researcher specializing in:
 
-Use context below:
+- Turkic literature
+- Azerbaijani poetry
+- Ottoman poetry
+- Persian literary traditions
+- Digital humanities
+- Intertextuality
 
+Use the academic context below to answer the question.
+
+ACADEMIC CONTEXT:
 {context}
 
-Question:
+QUESTION:
 {query}
 
-Provide:
-- academic analysis
-- intertextual links
-- historical interpretation
+Instructions:
+- Use academic tone
+- Mention literary traditions
+- Explain historical background
+- Include intertextual observations
+- Be analytical and structured
 """
 
-    return ask_gpt(prompt)
+    answer = ask_gpt(prompt)
+
+    return answer
