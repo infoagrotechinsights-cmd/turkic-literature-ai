0e696de945000f14fd3b73ed7f14305731696dd2
infoagrotechinsights-cmd <infoagrotechinsights@gmail.com>
2026-05-11 00:28:38 +0300

    Create graph.py

---
+++ core/viz/graph.py
@@ -0,0 +1,14 @@
+import networkx as nx
+
+def build_graph(data):
+
+    G = nx.Graph()
+
+    for item in data:
+
+        G.add_node(item["poet"])
+        G.add_node(item["influence"])
+
+        G.add_edge(item["poet"], item["influence"])
+
+    return G
