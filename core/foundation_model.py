a26b46eac68b799af2a727e7270d831b5f7776c7
infoagrotechinsights-cmd <infoagrotechinsights@gmail.com>
2026-05-11 03:14:23 +0300

    Update foundation_model.py

--- core/foundation_model.py
+++ core/foundation_model.py
@@ -5,27 +5,19 @@ from core.intertext import find_intertext
 def foundation_reasoning(poem: str):
 
     alignment = align_poem(poem)
-    intertext = find_intertext(poem)
 
-    motifs = extract_motifs(intertext)
+    intertext = find_intertext(poem)
 
     return {
         "alignment": alignment,
         "intertext": intertext,
-        "motifs": motifs,
+        "motifs": extract_motifs(intertext),
         "citations": []
     }
 
 
 def extract_motifs(intertext):
-
-    motifs = []
-
-    for item in intertext:
-
-        motifs.append({
-            "term": item.get("term"),
-            "type": item.get("type")
-        })
-
-    return motifs
+    return [
+        {"term": i.get("term"), "type": i.get("type")}
+        for i in intertext
+    ]
